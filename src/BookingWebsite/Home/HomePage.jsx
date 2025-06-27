d778d5798a16e68345ade7fa089d946563010b32
Karthikn-VR <karthikeyan33607@gmail.com>
2025-06-27 17:21:11 +0530

    Update HomePage.jsx

--- src/BookingWebsite/Home/HomePage.jsx
+++ src/BookingWebsite/Home/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Navbar, Nav, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Home from '../Images/Home.png'
 import logo from '../Images/RIDEZY.png';
 import HomeText from './HomeText';
