9d8c734c988b13ca4df758d2ee893abe2a9c7348
Karthikn-VR <karthikeyan33607@gmail.com>
2025-06-27 17:09:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,8 @@ import { BrowserRouter } from 'react-router-dom';
 export default function App() {
 
   return(
-<<<<<<< HEAD
-    <BrowserRouter basename="/react-mysql-bus_booking_website/">
-=======
     <BrowserRouter basename="/react-mysql-bus_booking_website/" >
->>>>>>> d33152c5ffae77064e8f65a64df1baba68bd169d
+
     <Routes>
       <Route path='/' element={<Indexpage />}></Route>
       <Route path='/Bookticket' element={<BookTicket />}></Route>
